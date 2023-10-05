71c17d6af09bf06cbe00ee4a9d6b70e7dfdb0e67
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2023-10-05 14:56:35 +1100

    clean up

--- app/src/main/java/com/nirvikbasnet/pdfreader/MainActivity.kt
+++ app/src/main/java/com/nirvikbasnet/pdfreader/MainActivity.kt
@@ -5,19 +5,10 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.compose.rememberNavController
 import com.nirvikbasnet.pdfreader.ui.navigation.PdfReaderNavigation
-import com.nirvikbasnet.pdfreader.ui.navigation.bottomNavigation.BottomNavBar
 import com.nirvikbasnet.pdfreader.ui.screen.viewModel.DownloadPdfViewModel
 import com.nirvikbasnet.pdfreader.ui.screen.viewModel.PdfListViewModel
 
