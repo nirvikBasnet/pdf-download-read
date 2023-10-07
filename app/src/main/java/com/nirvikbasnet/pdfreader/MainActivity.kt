d212e02f218ee80043f6068a22fb914e9a8f8480
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2023-10-07 19:00:48 +1100

    bottom navigation bar fixed

--- app/src/main/java/com/nirvikbasnet/pdfreader/MainActivity.kt
+++ app/src/main/java/com/nirvikbasnet/pdfreader/MainActivity.kt
@@ -1,39 +1,32 @@
 package com.nirvikbasnet.pdfreader
 
 import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
+import com.nirvikbasnet.pdfreader.data.PreferencesManager
 import com.nirvikbasnet.pdfreader.ui.navigation.PdfReaderNavigation
+import com.nirvikbasnet.pdfreader.ui.navigation.bottomNavigation.BottomNavBar
+import com.nirvikbasnet.pdfreader.ui.screen.MainScreen
 import com.nirvikbasnet.pdfreader.ui.screen.viewModel.DownloadPdfViewModel
 import com.nirvikbasnet.pdfreader.ui.screen.viewModel.PdfListViewModel
 
 class MainActivity : ComponentActivity() {
 
-    private val dir : Uri
-        get() {
-            TODO()
-        }
-
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Scaffold(
 
-                ){
-                    val downloadPdfViewModel = ViewModelProvider(this).get(DownloadPdfViewModel::class.java)
-                    val pdfListViewModel = ViewModelProvider(this).get(PdfListViewModel::class.java)
-
-                    PdfReaderNavigation(it,dir,downloadPdfViewModel,pdfListViewModel)
-
-                }
-
+            MainScreen()
 
             }
         }
